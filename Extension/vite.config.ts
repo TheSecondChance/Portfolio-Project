18eba2ff5ce7ebf5c68a1b752057fef73d9e682e
TheSecondChance <mesakmegerem@gmail.com>
2024-07-04 01:53:56 +0300

    modify vite to bundle two dirs

--- Extension/vite.config.ts
+++ Extension/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@question": path.resolve(__dirname, "./question"),
+    },
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        question: path.resolve(__dirname, "question/question.html"),
+      },
+    },
+  },
+});
