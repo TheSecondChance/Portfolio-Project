7296539c691e76c9b85e188ef665321eb4469c7b
TheSecondChance <mesakmegerem@gmail.com>
2024-07-04 00:26:21 +0300

    add manifest.json

---
+++ Extension/public/background.js
@@ -0,0 +1,35 @@
+let alarmInterval = null;
+
+function createAlarm() {
+  if (alarmInterval !== null) {
+    chrome.alarms.create("popupReminder", { periodInMinutes: alarmInterval });
+  }
+}
+
+chrome.runtime.onInstalled.addListener(() => {
+  createAlarm();
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "popupReminder") {
+    chrome.windows.create({
+      url: "./question/question.html",
+      type: "popup",
+      width: 600,
+      height: 310,
+    });
+    createAlarm();
+  }
+});
+
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.type === "setAlarmInterval") {
+    if (message.interval === 0) {
+      chrome.alarms.clear("popupReminder");
+      alarmInterval = null;
+    } else {
+      alarmInterval = message.interval;
+      createAlarm();
+    }
+  }
+});
