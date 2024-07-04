ac2c41593140b269e2b4e01c360c57df58174dad
Josh <mesakmegerem@gmail.com>
2024-07-04 03:47:04 +0300

    add admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import User, Category, Question, Answer
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "categories"),
+            },
+        ),
+    )
+
+admin.site.register(Category)
+
+class AnswerAdmin(admin.StackedInline):
+    model = Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerAdmin]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
